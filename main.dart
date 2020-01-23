bd73e1a9a4ec7932362f47b43ae9939006dd3446
Joao Pedro <laharlx@hotmail.com>
2020-01-23 11:22:23 -0300

    Testes na classe Triangulo

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'triangulo.dart';
+
+main(){
+  //int a,b,c, menor1,menor2,maior;
+  //menor1=2;
+  //menor2=3;
+  //maior=4;
+  //a=2;
+  //b=2;
+  //c=1;
+  //validacao(menor1, menor2, maior);
+  //ladosTriangulos(a, b, c);
+  
+}
